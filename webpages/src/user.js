75d3e2039b675e0ad1e95525dd8d7a19d574c5ba
Laney Tran <laneytran@MacBook-Pro.local>
2022-04-18 13:47:09 -0500

    edited user.html

--- webpages/src/user.js
+++ webpages/src/user.js
@@ -1,3 +1,4 @@
+//navigate between tabs
 const tabLinks = document.querySelectorAll(".tabs a");
 const tabPanels = document.querySelectorAll(".tabs-panel");
 
@@ -15,7 +16,73 @@ for (let el of tabLinks) {
     const panel = [...tabPanels].filter(el => el.getAttribute("data-index") == index);
     panel[0].classList.add("active");
     });
-  }
+}
+
+  //send to edit profile/settings tab
+const editProfile = document.querySelector(".chatbtn");
+const settings = document.querySelector("#settings-tab")
+const tabInfo = document.querySelectorAll(".tabs-panel");
+
+editProfile.addEventListener("click", e => {
+      let tab = tabLinks[4]
+      let info = tabInfo
+
+      e.preventDefault();
+  
+      document.querySelector(".tabs li.active").classList.remove("active");
+      document.querySelector(".tabs-panel.active").classList.remove("active");
+  
+      const parentListItem = tab.parentElement;
+      parentListItem.classList.add("active");
+      const index = [...parentListItem.parentElement.children].indexOf(parentListItem);
+  
+      const panel = [...info].filter(tab => tab.getAttribute("data-index") == index);
+      panel[0].classList.add("active");
+  });
+
+const settingsTab = document.querySelector("#idtopbar");
+settingsTab.addEventListener("click", e => {
+  let tab = tabLinks[4]
+  let info = tabInfo
+
+  e.preventDefault();
+
+  document.querySelector(".tabs li.active").classList.remove("active");
+  document.querySelector(".tabs-panel.active").classList.remove("active");
+
+  const parentListItem = tab.parentElement;
+  parentListItem.classList.add("active");
+  const index = [...parentListItem.parentElement.children].indexOf(parentListItem);
+
+  const panel = [...info].filter(tab => tab.getAttribute("data-index") == index);
+  panel[0].classList.add("active");
+});
+
+//send to upload page
+const uploadButton = document.querySelector("button.createbtn");
+const uploadTab = document.querySelector("#uploadtopbar");
+
+uploadTab.addEventListener("click", e => {
+  let tab = tabLinks[0]
+  let info = tabInfo
+
+  e.preventDefault();
+
+  document.querySelector(".tabs li.active").classList.remove("active");
+  document.querySelector(".tabs-panel.active").classList.remove("active");
+
+  const parentListItem = tab.parentElement;
+  parentListItem.classList.add("active");
+  const index = [...parentListItem.parentElement.children].indexOf(parentListItem);
+
+  const panel = [...info].filter(tab => tab.getAttribute("data-index") == index);
+  panel[0].classList.add("active");
+});
+
+uploadTab.onclick = (()=>{
+  uploadButton.click();
+  return false;
+});
 
 function deleteCookie(name) {
     document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
